78f643c2accfd9ea52e240869a3b8ececc074bdc
Niko Ware <wrnkt1@gmail.com>
2023-12-25 03:35:52 -0500

    Init

---
+++ 2023/java/MainThree.java
@@ -0,0 +1,198 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+class Point {
+  private int X;
+  private int Y;
+
+  public Point() {
+    this(0, 0);
+  }
+
+  public Point(int X, int Y) {
+    this.X = X;
+    this.Y = Y;
+  }
+
+  public int getX() {
+    return X;
+  }
+
+  public int getY() {
+    return Y;
+  }
+
+  public void setX(int X) {
+    this.X = X;
+  }
+
+  public void setY(int Y) {
+    this.Y = Y;
+  }
+
+  public boolean isAdjacent(Point other) {
+    int lowX = this.X - 1;
+    int highX = this.X + 1;
+    int lowY = this.Y - 1;
+    int highY = this.Y + 1;
+    if ((other.getX() >= lowX && other.getX() <= highX) &&
+        (other.getY() >= lowY && other.getY() <= highY)) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class Number {
+  private int value;
+  private StringBuilder literal = new StringBuilder();
+  private ArrayList<Point> location = new ArrayList<>();
+  private boolean isPartNumber = false;
+
+  public void addDigit(char c, int col, int row) {
+    literal.append(c);
+    updateValue();
+    location.add(new Point(col, row));
+  }
+
+  private void updateValue() {
+    value = Integer.parseInt(literal.toString());
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public boolean isPartNumber() {
+    return isPartNumber;
+  }
+
+  private boolean adjacentTo(Point point) {
+    for (Point p : location) {
+      if (point.isAdjacent(p))
+        return true;
+    }
+    return false;
+  }
+
+  public void checkPartNumber(Point point) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(String.format("Checking Number with points:\n"));
+    for (Point p : this.location) {
+      sb.append(String.format("\t(%d, %d)\n", p.getX(), p.getY()));
+    }
+    sb.append(String.format("Against Point: (%d, %d)\n", point.getX(), point.getY()));
+    System.out.println(sb.toString());
+    if (adjacentTo(point)) {
+      System.out.println("ADJACENT");
+      this.isPartNumber = true;
+    }
+  }
+
+  public void print() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(String.format("Value: %d\n", this.value));
+    sb.append(String.format("Literal: %s\n", this.literal.toString()));
+    sb.append(String.format("Points:\n", this.value));
+    for (Point p : location) {
+      sb.append(String.format("\t(%d, %d)\n", p.getX(), p.getY()));
+    }
+    sb.append(String.format("isPartNumber: %b\n", this.isPartNumber));
+    System.out.println(sb.toString());
+  }
+}
+
+public class MainThree {
+
+  public static boolean isSymbol(Character c) {
+    if (Character.isDigit(c) || c == '.') {
+      return false;
+    }
+    return true;
+  }
+
+  public static ArrayList<Number> readInNumbers(Path path) {
+    ArrayList<Number> numbers = new ArrayList<>();
+    try {
+      BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+
+      boolean processingNumber = false;
+      Number curNumber = null;
+
+      String line;
+      int row = 0;
+      while ((line = reader.readLine()) != null) {
+
+        for (int col = 0; col < line.length(); ++col) {
+
+          char c = line.charAt(col);
+
+          if (Character.isDigit(c)) {
+            if (!processingNumber) {
+              processingNumber = true;
+              curNumber = new Number();
+              numbers.add(curNumber);
+            }
+            curNumber.addDigit(c, col, row);
+
+          } else {
+            if (processingNumber) {
+              processingNumber = false;
+            }
+          }
+        }
+        ++row;
+      }
+    } catch (IOException e) {
+
+    }
+    return numbers;
+  }
+
+  public static void checkPartNumber(Path path, ArrayList<Number> numbers) {
+    try {
+      BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+      int row = 0;
+      String line;
+      while ((line = reader.readLine()) != null) {
+
+        for (int col = 0; col < line.length(); ++col) {
+          char c = line.charAt(col);
+
+          if (isSymbol(c)) {
+            Point point = new Point(col, row);
+            numbers.stream().forEach((n) -> n.checkPartNumber(point));
+          }
+        }
+        ++row;
+      }
+    } catch (IOException e) {
+
+    }
+  }
+
+  public static ArrayList<Number> processFile(Path path) {
+    ArrayList<Number> numbers = readInNumbers(path);
+    checkPartNumber(path, numbers);
+    return numbers;
+  }
+
+  public static void main(String... args) {
+    String filePath = "../input/3.txt";
+    Path path = Paths.get(filePath);
+
+    ArrayList<Number> nums = processFile(path);
+
+    Stream<Number> partNumbers = nums.stream().filter((n) -> n.isPartNumber());
+
+    Integer sum = partNumbers.mapToInt((pn) -> pn.getValue()).sum();
+    System.out.println(String.format("Sum: %d", sum));
+  }
+
+}
---
+++ 2023/java/MainTwo.java
@@ -0,0 +1,176 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+enum Color {
+  BLUE, RED, GREEN
+}
+
+record GameInfo(int id, ArrayList<Map<Color, Integer>> handfuls) {
+}
+
+public class MainTwo {
+
+  public static final Pattern idPattern = Pattern.compile("Game (?<id>\\d+)");
+  public static final Pattern numPattern = Pattern.compile("(\\d+)");
+
+  public static void printGameInfo(GameInfo game) {
+    System.out.println("ID: " + game.id());
+    int count = 1;
+    for (Map<Color, Integer> handful : game.handfuls()) {
+      System.out.println(String.format("Handful %d: ", count));
+      System.out.println();
+      for (Map.Entry<Color, Integer> colorCount : handful.entrySet()) {
+        System.out.println(String.format("\t%s: %d", colorCount.getKey(), colorCount.getValue()));
+      }
+      count++;
+    }
+  }
+
+  private static ArrayList<Map<Color, Integer>> processGameColors(final String s) {
+    ArrayList<Map<Color, Integer>> handfullList = new ArrayList<>();
+
+    String handfullsString = s.split(":")[1].strip();
+    String[] handfullsArr = handfullsString.split(";", 0);
+
+    for (String handfull : handfullsArr) {
+      String[] colorArr = handfull.split(",", 0);
+      Map<Color, Integer> handfulMap = new HashMap<>();
+      for (String colorCount : colorArr) {
+        int num = 0;
+        Color color = null;
+
+        Matcher m = numPattern.matcher(colorCount);
+
+        if (!m.find()) {
+          System.out.println("[ERROR]: big error panic");
+        }
+
+        num = Integer.parseInt(m.group(0));
+
+        if (colorCount.contains("blue")) {
+          color = Color.BLUE;
+        } else if (colorCount.contains("red")) {
+          color = Color.RED;
+        } else if (colorCount.contains("green")) {
+          color = Color.GREEN;
+        }
+
+        handfulMap.put(color, Integer.valueOf(num));
+      }
+      handfullList.add(handfulMap);
+    }
+    return handfullList;
+
+  }
+
+  private static int processGameId(final String s) {
+
+    Matcher idMatcher = idPattern.matcher(s);
+    String strId;
+    Integer id = 0;
+
+    if (idMatcher.find()) {
+      strId = idMatcher.group("id");
+      id = Integer.parseInt(strId);
+    }
+
+    return id;
+  }
+
+  private static GameInfo processGame(final String s) {
+    GameInfo g = new GameInfo(processGameId(s), processGameColors(s));
+    return g;
+  }
+
+  private static ArrayList<GameInfo> loadGamesFromFile(Path path) {
+    ArrayList<GameInfo> games = new ArrayList<>();
+    try {
+      BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+      String line;
+      while ((line = reader.readLine()) != null) {
+        games.add(processGame(line));
+      }
+    } catch (IOException e) {
+      System.out.println(String.format("[ERROR]: could not read file %d", path.toString()));
+    }
+    return games;
+  }
+
+  private static boolean validGame(Map<Color, Integer> bagContents, GameInfo game) {
+    for (Map<Color, Integer> handful : game.handfuls()) {
+      for (Map.Entry<Color, Integer> colorCount : handful.entrySet()) {
+        Color color = colorCount.getKey();
+        Integer count = colorCount.getValue();
+        if (count > bagContents.get(color))
+          return false;
+      }
+    }
+    return true;
+  }
+
+  private static Map<Color, Integer> minimumSet(GameInfo game) {
+    Map<Color, Integer> minimumSet = new HashMap<>();
+    minimumSet.put(Color.RED, 0);
+    minimumSet.put(Color.GREEN, 0);
+    minimumSet.put(Color.BLUE, 0);
+    for (Map<Color, Integer> handful : game.handfuls()) {
+      for (Map.Entry<Color, Integer> colorCount : handful.entrySet()) {
+        Color color = colorCount.getKey();
+        Integer count = colorCount.getValue();
+        if (minimumSet.get(color) < count) {
+          minimumSet.put(color, count);
+        }
+      }
+    }
+    return minimumSet;
+  }
+
+  private static Integer powerSet(Map<Color, Integer> set) {
+    int power = 1;
+    for (Map.Entry<Color, Integer> colorCount : set.entrySet()) {
+      Integer count = colorCount.getValue();
+      power *= count;
+    }
+    return power;
+  }
+
+  private static void partOne(ArrayList<GameInfo> games) {
+    Map<Color, Integer> bagContents = new HashMap<>();
+    bagContents.put(Color.RED, 12);
+    bagContents.put(Color.GREEN, 13);
+    bagContents.put(Color.BLUE, 14);
+
+    Stream<GameInfo> validGames = games.stream().filter((g) -> validGame(bagContents, g));
+    Integer sumValidIds = validGames.mapToInt((g) -> g.id()).sum();
+
+    System.out.println(String.format("Sum of valid game IDs: %d", sumValidIds));
+  }
+
+  private static void partTwo(ArrayList<GameInfo> games) {
+    Integer sumOfPowerSet = games.stream()
+        .map((g) -> minimumSet(g))
+        .mapToInt((s) -> powerSet(s))
+        .sum();
+    System.out.println(String.format("Sum of powers: %d", sumOfPowerSet));
+  }
+
+  public static void main(String... args) {
+    String filePath = "../input/2.txt";
+    Path path = Paths.get(filePath);
+
+    ArrayList<GameInfo> games = loadGamesFromFile(path);
+
+    partOne(games);
+    partTwo(games);
+  }
+}
