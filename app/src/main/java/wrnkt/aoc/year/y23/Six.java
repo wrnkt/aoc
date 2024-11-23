5d328f77fbc558ea923161247b48c01840b4fda3
Niko Ware <wrnkt@icloud.com>
2024-11-22 22:56:13 -0500

    solution: Start 2023 Day6 Part2

--- app/src/main/java/wrnkt/aoc/year/y23/Six.java
+++ app/src/main/java/wrnkt/aoc/year/y23/Six.java
@@ -3,10 +3,7 @@ package wrnkt.aoc.year.y23;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Stream;
 
 import wrnkt.aoc.util.Day;
 
@@ -26,8 +23,8 @@ public class Six implements Day {
                 try {
                     races.add(
                         new Race(
-                            Integer.parseInt(timeReadings[i]), 
-                            Integer.parseInt(distanceReadings[i])
+                            Long.parseUnsignedLong(timeReadings[i]), 
+                            Long.parseUnsignedLong(distanceReadings[i])
                         )
                     );
                 } catch (NumberFormatException e) {
@@ -48,6 +45,11 @@ public class Six implements Day {
             int waysToWin = waysToWin(race);
             waysToWinPerRace.add(waysToWin);
             log.info("{} -> {} ways to win", race.toString(), waysToWin);
+            List<Boolean> holdTimeStats = holdTimeStats(race);
+            for (int i = 0; i < holdTimeStats.size(); i++) {
+                log.info("Race results:");
+                log.info("holdTIme {} ms{ }", i, (holdTimeStats.get(i) ? "wWON" : "" ));
+            }
         }
 
         var answerOpt = races.stream()
@@ -55,42 +57,77 @@ public class Six implements Day {
                         .reduce((a, b) -> a * b);
 
         answerOpt.ifPresent((answer) -> {
-            log.info("Part 1 answer: {}", answer);
+            log.info("Part 1: {}", answer);
         });
     }
 
     public void partTwo(List<Race> races) {
+        Object time = races.stream()
+                .map(r ->  r.allowedTime())
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+        assert (time instanceof StringBuilder);
+        time = Long.parseUnsignedLong(time.toString());
 
+        Object distance = races.stream()
+                .map(r -> r.bestRecordedDistance())
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+        assert (distance instanceof StringBuilder);
+        distance = Long.parseUnsignedLong(distance.toString());
+
+        var newRace = new Race((long) time, (long) distance);
+
+        log.info("Part 2: {}");
     }
 
     public int waysToWin(Race race) {
         int wins = 0;
 
         for (int holdTime = 0; holdTime < race.allowedTime(); holdTime++) {
-            int distanceTraveled = Boat.travelDistance(holdTime, race.allowedTime());
+            long distanceTraveled = Boat.travelDistance(holdTime, race.allowedTime());
             if (distanceTraveled > race.bestRecordedDistance()) wins++;
         }
 
         return wins;
     }
 
+    public List<Boolean> holdTimeStats(Race race) {
+        List<Boolean> holdTimeStats = new ArrayList<>();
+
+        var didWin = false;
+        for (int holdTime = 0; holdTime < race.allowedTime(); holdTime++) {
+            long distanceTraveled = Boat.travelDistance(holdTime, race.allowedTime());
+            if (distanceTraveled > race.bestRecordedDistance()) didWin = true;
+
+            holdTimeStats.add(holdTime, didWin);
+
+        }
+        assert (holdTimeStats.size() == race.allowedTime());
+        return holdTimeStats;
+    }
+
     @Override
     public void solution(BufferedReader reader) {
         List<Race> races = readRaces(reader);
         partOne(races);
+        partTwo(races);
     }
 
 }
 
 class Boat {
-    public static int travelDistance(int buttonHoldMs, int maxTime) {
-        int travelTime = maxTime - buttonHoldMs;
-        int speed = buttonHoldMs;
+    public static long travelDistance(long buttonHoldMs, long maxTime) {
+        long travelTime = maxTime - buttonHoldMs;
+        long speed = buttonHoldMs;
         return travelTime * speed;
     }
 }
 
-record Race(int allowedTime, int bestRecordedDistance) {
+record Race(long allowedTime, long bestRecordedDistance) {
+
+    public Race(String time, String distance) {
+        this(Long.parseUnsignedLong(time), Long.parseUnsignedLong(distance));
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
