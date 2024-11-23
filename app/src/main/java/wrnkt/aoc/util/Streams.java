06a6bec3dfa191bb8edebd99d13eebd7c52fbc08
Niko Ware <wrnkt@icloud.com>
2024-11-22 21:24:15 -0500

    feat: Add streams utilities and Pair

---
+++ app/src/main/java/wrnkt/aoc/util/Streams.java
@@ -0,0 +1,26 @@
+package wrnkt.aoc.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+import wrnkt.aoc.util.Pair;
+
+public interface Streams {
+
+    public static <A, B> Stream<Pair<A, B>> zip(Stream<A> streamA, Stream<B> streamB) {
+        Iterator<A> iteratorA = streamA.iterator();
+        Iterator<B> iteratorB = streamB.iterator();
+
+        List<Pair<A, B>> list = new ArrayList<>();
+
+        while (iteratorA.hasNext() && iteratorB.hasNext()) {
+            A elemA = iteratorA.next();
+            B elemB = iteratorB.next();
+            list.add(new Pair<>(elemA, elemB));
+        }
+        return list.stream();
+    }
+
+}
