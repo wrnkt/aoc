7a06272c5b00b423ae32ffc7542202308eee05aa
Niko Ware <wrnkt@icloud.com>
2025-01-30 00:27:02 -0500

    Fix parsing (missing attempt to match call)

--- java/src/main/java/wrnkt/aoc/year/y15/Six.java
+++ java/src/main/java/wrnkt/aoc/year/y15/Six.java
@@ -1,14 +1,19 @@
 package wrnkt.aoc.year.y15;
 
 import java.io.BufferedReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import aoc.framework.Day;
+import wrnkt.aoc.year.y15.Six.Instruction.ACTION;
 
 public class Six extends Day {
+
     @Override
     public void solution(BufferedReader reader) {
         List<Instruction> instructions = readData(reader);
@@ -20,6 +25,8 @@ public class Six extends Day {
         Lights lights = new Lights();
         instructions.stream()
                     .forEachOrdered(instruction -> lights.execute(instruction));
+        long lightsOnCount = lights.lightsOn();
+        print(String.format("Lights on: %d", lightsOnCount));
     }
 
     public List<Instruction> readData(BufferedReader reader) {
@@ -31,24 +38,58 @@ public class Six extends Day {
     class Lights {
 
         boolean[][] grid = new boolean[1000][1000];
+        List<List<Boolean>> grid2 = new ArrayList<>();
 
         public Lights() {
             init();
+            for (int i = 0; i < 1000; i++) {
+                List<Boolean> row = new ArrayList<>();
+                grid2.add(
+                    Stream.generate(() -> false).limit(1000).collect(Collectors.toList())
+                );
+            }
         }
 
-        public void execute(Instruction inst) {
-            for (int i = inst.a.x; i < inst.b.x; i++) {
-                for (int j = inst.a.y; j < inst.b.y; j++) {
+        public void execute2(Instruction inst) {
+            for (int i = inst.a.x; i <= inst.b.x; i++) {
+                for (int j = inst.a.y; j <= inst.b.y; j++) {
+                    Boolean light = grid2.get(i).get(j);
                     switch (inst.action) {
                         case ON:
-                            grid[i][j] = true;
+                            light = Boolean.TRUE;
+                            break;
                         case OFF:
-                            grid[i][j] = false;
+                            light = Boolean.FALSE;
+                            break;
                         case TOGGLE:
-                            grid[i][j] = !grid[i][j];
+                            light = !light;
+                            break;
                     }
                 }
             }
+        }
+
+        public void execute(Instruction inst) {
+            long lightsChanged = 0;
+            for (int i = inst.a.x; i <= inst.b.x; i++) {
+                for (int j = inst.a.y; j <= inst.b.y; j++) {
+                    boolean currentLightState = grid[i][j];
+
+                    if (inst.action == ACTION.ON)
+                        grid[i][j] = true;
+                    else if (inst.action == ACTION.OFF)
+                        grid[i][j] = false;
+                    else if (inst.action == ACTION.TOGGLE)
+                        grid[i][j] = !grid[i][j];
+                    else 
+                        throw new IllegalArgumentException("Unknown instruction type");
+
+                    if (currentLightState != grid[i][j])
+                        lightsChanged++;
+                }
+            }
+            print("Instruction: " + inst.toString());
+            print(String.format("lightsChanged: %d", lightsChanged));
         }
 
         public void init() {
@@ -80,18 +121,26 @@ public class Six extends Day {
             setFrom(s);
         }
 
-        private static final Pattern INPUT_PATTERN = Pattern.compile("(turn on|turn off|toggle) (\\d+),(\\d+) through (\\d+),(\\d+)");
+        private static final Pattern INPUT_PATTERN = 
+            Pattern.compile("(turn on|turn off|toggle) (\\d+),(\\d+) through (\\d+),(\\d+)");
 
         private void setFrom(String s) {
             Matcher m = INPUT_PATTERN.matcher(s);
-            print(m.group(1));
-            print(m.group(2));
-            print(m.group(3));
-            print(m.group(4));
-            print(m.group(5));
+            if (!m.matches()) {
+                throw new IllegalArgumentException("Invalid instruction string provided: " + s);
+            }
             this.action = ACTION.parse(m.group(1));
             this.a = new Coordinate(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
             this.b = new Coordinate(Integer.parseInt(m.group(4)), Integer.parseInt(m.group(5)));
+        }
+
+        @Override
+        public String toString() {
+            return new StringBuilder()
+                .append(action)
+                .append(": ")
+                .append(String.format("(%d, %d) -> (%d, %d)", a.x, a.y, b.x, b.y))
+                .toString();
         }
 
         enum ACTION {
